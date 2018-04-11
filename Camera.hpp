373c148143eac3297a510f76962d7d9c95974e0f
DESKTOP-TC4AOHJ_momijzi <l7momip1z6junin3y@outlook.jp>
2018-04-11 18:26:23 +0900

    カメラが正常に動作していることを確認、テクスチャのhppの作成は完了したがうまく描画できていない下のエラーコードが毎ループでている、これが原因だと思われる。 エラー解消次第、spriteの作成に取り掛かる。 ただ今回のテクスチャとスプライトの関係がDirectX9と違うためそこをいじっていく可能性あり。 あとファイルの読みこみに(L"include")可能にした。
    D3D11 WARNING: ID3D11DeviceContext::DrawIndexed: The Pixel Shader unit expects a Sampler to be set at Slot 0, but none is bound.
    This is perfectly valid, as a NULL Sampler maps to default Sampler state. However, the developer may not want to rely on the defaults.
     [ EXECUTION WARNING #352: DEVICE_DRAW_SAMPLER_NOT_SET]

---
+++ Camera.hpp
@@ -0,0 +1,204 @@
+class Camera : public App::Window::Proceedable
+{
+public:
+	Float3 position;//カメラの座標
+	Float3 angles;//カメラの現在の見ている角度
+
+	Camera()
+	{
+		App::Initialize();
+
+		position = Float3(0.0f, 0.0f, -5.0f);
+		angles = Float3(0.0f, 0.0f, 0.0f);
+
+		SetPerspective(60.0f, 0.1f, 1000.0f);
+
+		App::AddProcedure(this);
+
+		Create();
+	}
+
+	~Camera()
+	{
+		App::RemoveProcedure(this);
+	}
+	//カメラの設定
+	void SetPerspective(float fieldOfView, float nearClip, float farClip)
+	{
+		this->fieldOfView = fieldOfView;
+		this->nearClip = nearClip;
+		this->farClip = farClip;
+
+		constant.projection = DirectX::XMMatrixTranspose
+		(
+			DirectX::XMMatrixPerspectiveFovLH
+			(
+				DirectX::XMConvertToRadians(fieldOfView),//視野角
+				App::GetWindowSize().x / App::GetWindowSize().y,//ビュー空間のアスペクト比X:Y
+				nearClip,//クリッピング面の近面
+				farClip//				   遠面
+			)
+		);
+	}
+	void Update()
+	{
+		//ここで座標と角度を変わる
+		constant.view = DirectX::XMMatrixTranspose
+		(
+			DirectX::XMMatrixInverse
+			(
+				nullptr,
+				DirectX::XMMatrixRotationZ(DirectX::XMConvertToRadians(angles.z)) *
+				DirectX::XMMatrixRotationY(DirectX::XMConvertToRadians(angles.y)) *
+				DirectX::XMMatrixRotationX(DirectX::XMConvertToRadians(angles.x)) *
+				DirectX::XMMatrixTranslation(position.x, position.y, position.z)
+			)
+		);
+
+		App::GetGraphicsContext().UpdateSubresource(constantBuffer, 0, nullptr, &constant, 0, 0);
+		App::GetGraphicsContext().VSSetConstantBuffers(1, 1, &constantBuffer.p);
+		App::GetGraphicsContext().HSSetConstantBuffers(1, 1, &constantBuffer.p);
+		App::GetGraphicsContext().DSSetConstantBuffers(1, 1, &constantBuffer.p);
+		App::GetGraphicsContext().GSSetConstantBuffers(1, 1, &constantBuffer.p);
+		App::GetGraphicsContext().PSSetConstantBuffers(1, 1, &constantBuffer.p);
+
+		App::GetGraphicsContext().OMSetRenderTargets(1, &renderTargetView.p, depthStencilView);
+
+
+		static float color[4] = { 1.0f,1.0f,1.0f ,1.0f };
+		App::GetGraphicsContext().ClearRenderTargetView
+		(
+			renderTargetView, color
+		);
+		App::GetGraphicsContext().ClearDepthStencilView
+		(
+			depthStencilView,
+			D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL,
+			1.0f, 0
+		);
+	}
+
+private:
+	struct Constant
+	{
+		DirectX::XMMATRIX view;
+		DirectX::XMMATRIX projection;
+	};
+
+	float fieldOfView;//視野
+	float nearClip;//クリッピング面の近面
+	float farClip;//クリッピング面の遠面
+
+	Constant constant;
+	ATL::CComPtr<ID3D11RenderTargetView> renderTargetView = nullptr;
+	ATL::CComPtr<ID3D11DepthStencilView> depthStencilView = nullptr;
+	ATL::CComPtr<ID3D11Texture2D> renderTexture = nullptr;
+	ATL::CComPtr<ID3D11Texture2D> depthTexture = nullptr;
+	ATL::CComPtr<ID3D11Buffer> constantBuffer = nullptr;
+
+	void Create()
+	{
+		renderTexture.Release();
+		App::GetGraphicsMemory().GetBuffer
+		(
+			0, __uuidof(ID3D11Texture2D),
+			reinterpret_cast<void**>(&renderTexture)
+		);
+		renderTargetView.Release();
+		App::GetGraphicsDevice().CreateRenderTargetView(renderTexture, nullptr, &renderTargetView);
+
+		DXGI_SWAP_CHAIN_DESC swapChainDesc = {};
+		App::GetGraphicsMemory().GetDesc(&swapChainDesc);
+
+		//2Dテクスチャの設定
+		depthTexture.Release();
+		D3D11_TEXTURE2D_DESC textureDesc = {};
+		textureDesc.Width = static_cast<UINT>(App::GetWindowSize().x);		//テクスチャの幅（単位　テクセル）
+		textureDesc.Height = static_cast<UINT>(App::GetWindowSize().y);		//テクスチャの高さ（単位　テクセル）
+		textureDesc.MipLevels = 1;											//テクスチャ内のミップマップレベルの最大数
+		textureDesc.ArraySize = 1;											//テクスチャー配列内のテクスチャ―数
+		textureDesc.Format = DXGI_FORMAT_R32_TYPELESS;						//テクスチャフォーマット
+		textureDesc.SampleDesc.Count = swapChainDesc.SampleDesc.Count;		//ピクセル単位のマルチサプリングの数
+		textureDesc.SampleDesc.Quality = swapChainDesc.SampleDesc.Quality;	//イメージの品質レベル、品質が高いほどパフォーマンスは低下する
+		textureDesc.Usage = D3D11_USAGE_DEFAULT;							//テクスチャの読み込み内容また書き込み方法を識別する値
+		textureDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;					//パイプラインステージへのバインドに関するフラグ
+		textureDesc.CPUAccessFlags = 0;										//許可するCPUアクセスの種類を指定するフラグ（論理和可能）
+		textureDesc.MiscFlags = 0;											//他の一般性の低いリソースオプションを識別するフラグ
+		App::GetGraphicsDevice().CreateTexture2D//3Dテクスチャの配列を作成
+		(
+			&textureDesc,	//2Dテクスチャの記述へのポインタ
+			nullptr,		//サブリソースの記述へのポインタ（マルチサプリングされている場合はNULLにする）
+			&depthTexture	//作成するテクスチャへのポインタ
+		);
+
+		//深度ステンシルビューからアクセス可能なテクスチャーのサブリソースを設定
+		depthStencilView.Release();
+		D3D11_DEPTH_STENCIL_VIEW_DESC depthStancilViewDesc = {};
+		depthStancilViewDesc.Format = DXGI_FORMAT_D32_FLOAT;//深度ステンシルビューのフォーマット
+		if (swapChainDesc.SampleDesc.Count == 0)
+		{
+			depthStancilViewDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;//深度ステンシルリソースへのアクセス方法
+			depthStancilViewDesc.Texture2D.MipSlice = 0;//最初に使用するミップマップレベルのインデックス
+		}
+		else
+		{
+			depthStancilViewDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2DMS;
+		}
+		App::GetGraphicsDevice().CreateDepthStencilView
+		(
+			depthTexture,			//深度ステンシル サーフェスとして機能するリソースへのポインタ
+			&depthStancilViewDesc,	//深度ステンシルビューの記述へのポインタ
+			&depthStencilView		//ID3D11DepthStencilView へのポインターのアドレス
+		);
+
+		//バッファーの作成
+		constantBuffer.Release();
+		D3D11_BUFFER_DESC constantBufferDesc = {};
+		constantBufferDesc.ByteWidth = static_cast<UINT>(sizeof(Constant));	//バッファーのサイズ（バイト単位）
+		constantBufferDesc.Usage = D3D11_USAGE_DEFAULT;						//バッファーで想定される読み込み、書き込みの方法を識別する
+		constantBufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;			//バッファーをどのようにパイプラインにバインドするかを識別する
+		constantBufferDesc.CPUAccessFlags = 0;								//CPUのアクセスフラグ
+		//バッファー（頂点バッファー、インデックスバッファー、またはシェーダ定数バッファー）を作成する
+		App::GetGraphicsDevice().CreateBuffer(&constantBufferDesc, nullptr, &constantBuffer);
+	}
+	void OnProceed(HWND, UINT message, WPARAM, LPARAM) override
+	{
+		if (message != WM_SIZE)
+		{
+			return;
+		}
+		if (App::GetWindowSize().x <= 0.0f || App::GetWindowSize().y <= 0.0f)
+		{
+			return;
+		}
+		DXGI_SWAP_CHAIN_DESC swapChainDesc = {};
+		App::GetGraphicsMemory().GetDesc(&swapChainDesc);
+
+		ATL::CComPtr<ID3D11RenderTargetView> nullRenderTarget = nullptr;
+		ATL::CComPtr<ID3D11DepthStencilView> nullDepthStencil = nullptr;
+
+		App::GetGraphicsContext().OMSetRenderTargets
+		(
+			1,
+			&nullRenderTarget,
+			nullDepthStencil
+		);
+
+		renderTargetView.Release();
+		depthStencilView.Release();
+		renderTexture.Release();
+
+		App::GetGraphicsContext().Flush();//コマンドバッファー内のコマンドをGPUに送信します
+
+		//スワップチェーンのバックバッファーサイズ、フォーマット、バッファー数を変更する
+		App::GetGraphicsMemory().ResizeBuffers
+		(
+			swapChainDesc.BufferCount,//スワップチェーンのバッファー数
+			static_cast<UINT>(App::GetWindowSize().x),//バックバッファ―の新しい幅
+			static_cast<UINT>(App::GetWindowSize().y),//バックバッファーの新しい高さ
+			swapChainDesc.BufferDesc.Format, swapChainDesc.Flags//スワップチェーンの機能を示すフラグ
+		);
+		SetPerspective(fieldOfView, nearClip, farClip);
+		Create();
+	}
+};
---
+++ Texture.hpp
@@ -0,0 +1,196 @@
+class Texture
+{
+public:
+	Texture()
+	{
+		App::Initialize();
+	}
+	Texture(const wchar_t* const filePath)
+	{
+		App::Initialize();
+
+		Load(filePath);
+	}
+	~Texture()
+	{
+
+	}
+
+	void Load(const wchar_t* const filePath)
+	{
+		static ATL::CComPtr<IWICImagingFactory> factory = nullptr;
+		if (factory == nullptr)
+		{
+			CoCreateInstance//指定されたCLSIDに関連付けされたクラスの初期化されていないobjの作成
+			(
+				CLSID_WICImagingFactory,//オブジェクトの作成に使用されるデータ、コードに関連付けされたCLSID
+				nullptr,				//NULLの場合objが集約の一部として作成されないことを示す、
+										//NULLではないときは集約objのIUnknowmインタフェースへのポインタ
+				CLSCTX_INPROC_SERVER,	//新しく作成されたobjを管理するコードが実行されるコンテキスト
+				IID_PPV_ARGS(&factory)	//objとの通信に使用されるインターフェースの識別子への参照
+			);
+		}
+			
+
+		ATL::CComPtr<IWICBitmapDecoder> decoder = nullptr;
+
+		factory->CreateDecoderFromFilename//IWICBitmapDecoderクラスの新しいインスタンスを作成
+		(
+			filePath,						//作成またはオープンするobjの名前を指定するヌル終了文字列へのポインタ
+			0,								//優先デコーダのGUID。優先ベンダーが無い場合はNULLを使用
+			GENERIC_READ,					//オブジェクトへのアクセスの仕方の定義
+			WICDecodeMetadataCacheOnDemand,	//デーコードオプションの指定
+			&decoder						//あたらしいIWICBitmapDecoderへのポインタを受け取るポインタ
+		);
+
+		ATL::CComPtr<IWICBitmapFrameDecode> frame = nullptr;
+		decoder->GetFrame
+		(
+			0,		//取得する特定のフレーム
+			&frame	//IWICBitmapFrameDecodeへのポインタを受け取るポインタ
+		);
+		UINT width, height;
+		frame->GetSize//ビットマップのピクセル幅と高さの取得
+		(
+			&width,//ピクセル幅
+			&height//		 高さ
+		);
+
+		WICPixelFormatGUID pixelFormat;
+		frame->GetPixelFormat	//ビットマップソースのピクセル形式を取得します
+		(
+			&pixelFormat		//ビットマップが格納されているピクセル形式のGUIDを受け取る
+		);
+		std::unique_ptr<BYTE[]> buffer(new BYTE[width * height * 4]);
+
+		if (pixelFormat != GUID_WICPixelFormat32bppRGBA)
+		{
+			ATL::CComPtr<IWICFormatConverter> formatConverter = nullptr;
+			factory->CreateFormatConverter	//IWICFormatConverterクラスの新しいインスタンスを作成する
+			(
+				&formatConverter//新しいIWICFormatConverterへのポインタを取得するポインタ
+			);
+
+			formatConverter->Initialize//フォーマットコンバータを初期化する
+			(
+				frame,								//変換する入力ビットマップ
+				GUID_WICPixelFormat32bppRGBA,		//宛先ピクセル形式GUID
+				WICBitmapDitherTypeErrorDiffusion,	//画像フォーマット間で変換するときに適用されるディザアルゴリズムタイプの指定
+				0,									//変換に使用するパレット
+				0,									//変換に使用するアルファ闘値
+				WICBitmapPaletteTypeCustom			//変換に使用するパレット変換タイプ
+			);
+
+			formatConverter->CopyPixels//ビットマップのピクセルデータをコピーする
+			(
+				0,					//コピーする短形。NULLの場合、全体のビットマップを指定する
+				width * 4,			//ビットマップの横幅（wikiだとストライドだが……）
+				width* height * 4,	//バッファーのサイズ
+				buffer.get()		//バッファのポインタ
+			);
+		}
+		else
+		{
+			frame->CopyPixels
+			(
+				0,
+				width * 4,
+				width * height * 4,
+				buffer.get()
+			);
+		}
+		size = DirectX::XMINT2(width, height);
+		
+		//2Dテクステャについて
+		texture.Release();
+		D3D11_TEXTURE2D_DESC textureDesc = {};
+		textureDesc.Width = width;							//テクステャの幅（テクセル単位）
+		textureDesc.Height = height;						//テクスチャの高さ（テクセル単位）
+		textureDesc.MipLevels = 1;							//テクスチャ内のミップマップサイズレベルの最大数
+		textureDesc.ArraySize = 1;							//テクスチャの配列内のテクスチャの数
+		textureDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;	//テクスチャのフォーマット
+		textureDesc.SampleDesc.Count = 1;					//ピクセル単位のマルチサプリングの数
+		textureDesc.SampleDesc.Quality = 0;					//イメージの品質レベル。品質が高いほどパフォーマンスが低下する
+		textureDesc.Usage = D3D11_USAGE_DEFAULT;			//テクスチャの読み込みまた書き込み方法を識別する値
+		textureDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE;	//パイプラインステージへのバインドに関するフラグ
+		textureDesc.CPUAccessFlags = D3D11_CPU_ACCESS_READ;	//許可するCPUアクセスの種類を指定するフラグ
+		textureDesc.MiscFlags = 0;							//ほかの一般性の低いリソースオプションを識別するフラグ
+
+		//サブリソースの初期化に使用されるデータの指定
+		D3D11_SUBRESOURCE_DATA textureSubresourceData = {};
+		textureSubresourceData.pSysMem = buffer.get();					//初期化データへのポインタ
+		textureSubresourceData.SysMemPitch = width * 4;					//テクスチャによる一本の線の先端から隣の線までの距離（バイト単位）
+		textureSubresourceData.SysMemSlicePitch = width * height * 4;	//一つの深度レベルから隣の深度レベルまでの距離（バイト単位）
+		
+		//上のデータを使い2Dテクスチャの配列を作成する
+		App::GetGraphicsDevice().CreateTexture2D
+		(
+			&textureDesc,			//2Dテクスチャ記述のポインタ
+			&textureSubresourceData,//サブリソースの記述の配列のポインタ
+			&texture				//作成されるテクスチャへのポインタへのアドレス
+		);
+
+		//シェーダーリソースビューの設定
+		shaderResourceView.Release();
+		D3D11_SHADER_RESOURCE_VIEW_DESC shaderResourceViewDesc = {};
+		shaderResourceViewDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;				//表示フォーマットを指定する
+		shaderResourceViewDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;	//ビューのリソースタイプ
+		shaderResourceViewDesc.Texture2D.MipLevels = 1;							//テクスチャ内のミップサイズレベルの最大数
+		App::GetGraphicsDevice().CreateShaderResourceView						//シェーダのリソースビューを作成する
+		(
+			texture,				//シェーダへの入力として機能するリソースへのポインタ
+			&shaderResourceViewDesc,//シェーダリソースビュー設定のポインタ
+			&shaderResourceView		//ID3D11ShaderResourceViewへのポインタのアドレス
+		);
+
+		//サンプラーステート
+		samplerState.Release();
+		D3D11_SAMPLER_DESC samplerDesc = {};
+		samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;	//テクスチャのサンプリング時に使用するフィルタリングメソッド
+		samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;		//0 ～ 1の範囲外にあるuテクスチャー座標を解決するために必要なメソッド
+		samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;		//					v
+		samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;		//					w
+		samplerDesc.MipLODBias = 0.0f;							//計算されたミップマップレベルからのオフセット
+		samplerDesc.MaxAnisotropy = 1;							//Filterに_ANISOTROPICがついているときに使用されるクランプ値
+		samplerDesc.ComparisonFunc = D3D11_COMPARISON_ALWAYS;	//既存のサンプリングデータに対してデータを比較する関数
+		samplerDesc.BorderColor[0] = 0.0f;						//AddressU、AddressV、AddressWに D3D11_TEXTURE_ADDRESS_BORDER
+		samplerDesc.BorderColor[1] = 0.0f;						//が指定されているときに使用される境界の色、0.0 ～ 1.0の範囲で指定
+		samplerDesc.BorderColor[2] = 0.0f;						//
+		samplerDesc.BorderColor[3] = 0.0f;						//
+		samplerDesc.MinLOD = 0.0f;								//アクセスをクランプするミップマップサイズの下限
+		samplerDesc.MaxLOD = D3D11_FLOAT32_MAX;					//											上限
+		App::GetGraphicsDevice().CreateSamplerState				//テクスチャーのサプリング情報をカプセル化するサンプラーステートobjの作成		
+		(
+			&samplerDesc,	//サンプラーステートの記述へのポインタ
+			&samplerState	//作成されるサンプラーステートobjへのポインタのアドレス
+		);
+	} 
+	DirectX::XMINT2 GetSize() const
+	{
+		return size;
+	}
+	void Attach(int slot)
+	{
+		if (texture == nullptr)
+			return;
+
+		App::GetGraphicsContext().PSSetShaderResources//ピクセルシェーダステージにシェーダのリソースの配列をバインドする
+		(
+			slot,					//デバイスの配列の中でシェーダリソースの設定開始位置の、0から始まるインデックス
+			1,						//設定するシェーダリソースの数
+			&shaderResourceView.p	//デバイスに設定されるシェーダリソースビューインターフェースの配列
+		);
+		App::GetGraphicsContext().PSSetSamplers//サンプラーステートの配列をピクセルシェーダのパイプラインステージに設定する
+		(
+			slot,			//上に同じ
+			1,				//配列内のサンプラーの数
+			&samplerState.p	//サンプラーステートインタフェースの配列へのポインタ
+		);
+	}
+
+private:
+	DirectX::XMINT2 size;
+	ATL::CComPtr<ID3D11Texture2D> texture = nullptr;
+	ATL::CComPtr<ID3D11ShaderResourceView> shaderResourceView = nullptr;
+	ATL::CComPtr<ID3D11SamplerState> samplerState = nullptr;
+};
