146ef8a72d93fe2607a455dd83b5eb03191ecdae
DESKTOP-TC4AOHJ_momijzi <l7momip1z6junin3y@outlook.jp>
2018-04-11 18:24:55 +0900

    忘れられたApp Meshついでに

---
+++ App.hpp
@@ -0,0 +1,112 @@
+#pragma once
+
+#define OEMRESOURCE
+
+#include <forward_list>
+#include <memory>
+#include <vector>
+#include <Windows.h>
+#include <atlbase.h>
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+#include <Wincodec.h>
+
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+
+#define MAIN()	APIENTRY wWinMain(HINSTANCE, HINSTANCE, LPWSTR, int)
+
+#include "Utility.hpp"
+
+class App final//final調べろー
+{
+public:
+	#include "Window.hpp"
+	#include"Graphics.hpp"
+
+	static constexpr wchar_t* NAME = L"DirectX11";
+
+	App() = delete;
+	//すべての初期化が完了したときのみtrueを返す
+	static bool Refresh()
+	{
+		GetGraphics().Update();
+		return GetWindow().Update();
+	}
+	//初期化関数
+	static void Initialize()
+	{
+		static bool isInitializes = false;
+
+		if (!isInitializes)
+		{
+			CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+			_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+			isInitializes = true;
+		}
+	}
+	//ウィンドウ----------------------------------------
+	static HWND GetWindowHandle()
+	{
+		return GetWindow().GetHandle();
+	}
+	static DirectX::XMINT2 GetWindowSize()
+	{
+		return GetWindow().GetSize();
+	}
+	static void SetWindowSize(int width,int height)
+	{
+		return GetWindow().SetSize(width,height);
+	}
+	static wchar_t* const GetTitle()
+	{
+		return GetWindow().GetTitle();
+	}
+	static void SetTitle(const wchar_t* const title)
+	{
+		GetWindow().SetTitle(title);
+	}
+	static void SetFullScreen(bool isFullScreen)
+	{
+		GetWindow().SetFullScreen(isFullScreen);
+	}
+	static void AddProcedure(Window::Proceedable* const procedure)
+	{
+		GetWindow().AddProcedure(procedure);
+	}
+	static void RemoveProcedure(Window::Proceedable* const procedure)
+	{
+		GetWindow().RemoveProcedure(procedure);
+	}
+	//グラフィック--------------------------------------
+	static ID3D11Device& GetGraphicsDevice()
+	{
+		return GetGraphics().GetDevice();
+	}
+	static ID3D11DeviceContext& GetGraphicsContext()
+	{
+		return GetGraphics().GetContext();
+	}
+	static IDXGISwapChain& GetGraphicsMemory()
+	{
+		return GetGraphics().GetMemory();
+	}
+private:
+	//ウィンドウのデータを取得するためのポインタ―
+	static Window& GetWindow()
+	{
+		static std::unique_ptr<Window> window(new Window());
+		return *window.get();
+	}
+	//グラフィックのデータを取得するためのポインタ―
+	static Graphics& GetGraphics()
+	{
+		static std::unique_ptr<Graphics> graphics(new Graphics());
+		return *graphics.get();
+	}
+};
+#include"Texture.hpp"
+#include"Material.hpp"
+#include"Camera.hpp"
+#include"Mesh.hpp"
---
+++ Mesh.hpp
@@ -0,0 +1,270 @@
+
+
+class Mesh
+{
+public:
+
+	Float3 position;	//座標
+	Float3 angles;		//角度
+	Float3 scale;		//大きさ
+
+	std::vector<Vertex> vertices;
+	std::vector<UINT> indices;
+	Vertex setVertex[36] = {};
+	Material material;
+
+	Mesh()
+	{
+		App::Initialize();
+		
+		position = Float3(0.0f, 0.0f, 0.0f);
+		angles = Float3(0.0f, 0.0f, 0.0f);
+		scale = Float3(1.0f, 1.0f, 1.0f);
+
+		material = Material(L"Shader.hlsl");
+
+		SetCullingMode(D3D11_CULL_BACK);
+
+		CreateCube();
+
+		Apply();
+	}
+
+	~Mesh()
+	{
+
+	}
+
+	//トライアングルリスト作成
+	void CreateTriangle()
+	{
+		vertices.clear();
+
+		//ベクタに要素を追加
+		//ここをいじれば描画する物を変えることが可能だろう
+		//できれば引数を使って動的に作りたい
+		vertices.push_back({ Float3(0.0f, 1.0f, 0.0f), Float3(1.0f, 0.0f, 0.0f) });
+		vertices.push_back({ Float3(1.0f, -1.0f, 0.0f), Float3(0.0f, 1.0f, 0.0f) });
+		vertices.push_back({ Float3(-1.0f, -1.0f, 0.0f), Float3(0.0f, 0.0f, 1.0f) });
+	}
+
+	void CreatePlane(
+		Float2 size = Float2(0.5f, 0.5f),
+		Float3 offset = Float3(0.0f, 0.0f, 0.0f),
+		bool souldclear = true,
+		Float3 leftDirection = Float3(1.0f, 0.0f, 0.0f),
+		Float3 upDirection = Float3(0.0f, 1.0f, 0.0f),
+		Float3 forwardDirection = Float3(0.0f, 0.0f, 0.0f)
+	)
+	{
+		if (souldclear)
+		{
+			vertices.clear();
+			indices.clear();
+		}
+		leftDirection = DirectX::XMVector3Normalize(leftDirection);
+		upDirection = DirectX::XMVector3Normalize(upDirection);
+		forwardDirection = DirectX::XMVector3Normalize(forwardDirection);
+
+		vertices.push_back({ leftDirection * -size.x + upDirection *  size.y + offset,
+			-forwardDirection });
+		vertices.push_back({ leftDirection *  size.x + upDirection *  size.y + offset,
+			-forwardDirection });
+		vertices.push_back({ leftDirection * -size.x + upDirection * -size.y + offset,
+			-forwardDirection });
+		vertices.push_back({ leftDirection *  size.x + upDirection * -size.y + offset,
+			-forwardDirection });
+
+		size_t indexOffset = vertices.size() - 4;
+		indices.push_back(indexOffset + 0);
+		indices.push_back(indexOffset + 1);
+		indices.push_back(indexOffset + 2);
+		indices.push_back(indexOffset + 3);
+		indices.push_back(indexOffset + 2);
+		indices.push_back(indexOffset + 1);
+	}
+
+	void CreateCube(
+		Float2 size = Float2(0.5f, 0.5f),
+		bool souldClear = true
+	)
+	{
+		if (souldClear)
+		{
+			vertices.clear();
+			indices.clear();
+		}
+
+		CreatePlane(size, Float3(0.0f, 0.0f, -0.5f), false,
+			Float3(1.0f, 0.0f, 0.0f), Float3(0.0f, 1.0f, 0.0f), Float3(0.0f, 0.0f, 1.0f));
+		CreatePlane(size, Float3(0.0f, 0.0f, 0.5f), false,
+			Float3(-1.0f, 0.0f, 0.0f), Float3(0.0f, 1.0f, 0.0f), Float3(0.0f, 0.0f, -1.0f));
+		CreatePlane(size, Float3(0.5f, 0.0f, 0.0f), false,
+			Float3(0.0f, 0.0f, 1.0f), Float3(0.0f, 1.0f, 0.0f), Float3(-1.0f, 0.0f, 0.0f));
+		CreatePlane(size, Float3(-0.5f, 0.0f, 0.0f), false,
+			Float3(0.0f, 0.0f, -1.0f), Float3(0.0f, 1.0f, 0.0f), Float3(1.0f, 0.0f, 0.0f));
+		CreatePlane(size, Float3(0.0f, 0.5f, 0.0f), false,
+			Float3(1.0f, 0.0f, 0.0f), Float3(0.0f, 0.0f, 1.0f), Float3(0.0f, -1.0f, 0.0f));
+		CreatePlane(size, Float3(0.0f, -0.5f, 0.0f), false,
+			Float3(1.0f, 0.0f, 0.0f), Float3(0.0f, 0.0f, -1.0f), Float3(0.0f, 1.0f, 0.0f));
+	}
+
+
+	void CreateTriangleBox(float x, float y, float z)
+	{
+		vertices.clear();
+
+		//上面
+		setVertex[0] = { Float3(0.5f + x,-0.5f + y, 0.5f + z), Float3(0.0f, 1.0f,-0.0f) };//左上　前
+		setVertex[1] = { Float3(-0.5f + x,-0.5f + y, 0.5f + z), Float3(1.0f, 1.0f,-0.0f) };//右上　前
+		setVertex[2] = { Float3(0.5f + x,-0.5f + y,-0.5f + z), Float3(1.0f, 1.0f, 1.0f) };//左上　奥
+
+		setVertex[3] = { Float3(0.5f + x,-0.5f + y,-0.5f + z), Float3(0.0f, 1.0f, 1.0f) };//左上　奥
+		setVertex[4] = { Float3(-0.5f + x,-0.5f + y, 0.5f + z), Float3(1.0f, 1.0f, 1.0f) };//右上　前
+		setVertex[5] = { Float3(-0.5f + x,-0.5f + y,-0.5f + z), Float3(0.0f, 1.0f,-0.0f) };//右上　奥
+
+		//前面
+		setVertex[6] = { Float3(-0.5f + x,-0.5f + y, 0.5f + z), Float3(0.0f, 1.0f,-0.0f) };//右上　前
+		setVertex[7] = { Float3(0.5f + x,-0.5f + y, 0.5f + z), Float3(1.0f, 1.0f,-0.0f) };//左上　前
+		setVertex[8] = { Float3(-0.5f + x, 0.5f + y, 0.5f + z), Float3(1.0f, 1.0f, 1.0f) };//右下　前
+
+		setVertex[9] = { Float3(-0.5f + x, 0.5f + y, 0.5f + z), Float3(0.0f, 1.0f, 1.0f) };//右下　前
+		setVertex[10] = { Float3(0.5f + x,-0.5f + y, 0.5f + z), Float3(1.0f, 1.0f, 1.0f) };//左上　前
+		setVertex[11] = { Float3(0.5f + x, 0.5f + y, 0.5f + z), Float3(0.0f, 1.0f,-0.0f) };//左下　前
+
+		//奥側
+		setVertex[12] = { Float3(0.5f + x,-0.5f + y,-0.5f + z), Float3(0.0f, 1.0f,-0.0f) };//左上　奥
+		setVertex[13] = { Float3(-0.5f + x,-0.5f + y,-0.5f + z), Float3(1.0f, 1.0f,-0.0f) };//右上　奥
+		setVertex[14] = { Float3(0.5f + x, 0.5f + y,-0.5f + z), Float3(1.0f, 1.0f, 1.0f) };//左下　奥
+
+		setVertex[15] = { Float3(0.5f + x, 0.5f + y,-0.5f + z), Float3(0.0f, 1.0f, 1.0f) };//左下　奥
+		setVertex[16] = { Float3(-0.5f + x,-0.5f + y,-0.5f + z), Float3(1.0f, 1.0f, 1.0f) };//右上　奥
+		setVertex[17] = { Float3(-0.5f + x, 0.5f + y,-0.5f + z), Float3(0.0f, 1.0f,-0.0f) };//右下　奥
+
+		//正面　右	側面
+		setVertex[18] = { Float3(-0.5f + x,-0.5f + y,-0.5f + z), Float3(0.0f, 1.0f,-0.0f) };//右上　奥
+		setVertex[19] = { Float3(-0.5f + x,-0.5f + y, 0.5f + z), Float3(1.0f, 1.0f,-0.0f) };//右上　前
+		setVertex[20] = { Float3(-0.5f + x, 0.5f + y,-0.5f + z), Float3(1.0f, 1.0f, 1.0f) };//右下　奥
+
+		setVertex[21] = { Float3(-0.5f + x, 0.5f + y,-0.5f + z), Float3(0.0f, 1.0f, 1.0f) };//右下　奥
+		setVertex[22] = { Float3(-0.5f + x,-0.5f + y, 0.5f + z), Float3(1.0f, 1.0f, 1.0f) };//右上　前
+		setVertex[23] = { Float3(-0.5f + x, 0.5f + y, 0.5f + z), Float3(0.0f, 1.0f,-0.0f) };//右下　前
+
+		//正面　左　側面
+		setVertex[24] = { Float3(0.5f + x,-0.5f + y, 0.5f + z), Float3(0.0f, 1.0f,-0.0f) };//左上　前
+		setVertex[25] = { Float3(0.5f + x,-0.5f + y,-0.5f + z), Float3(1.0f, 1.0f,-0.0f) };//左上　奥
+		setVertex[26] = { Float3(0.5f + x, 0.5f + y, 0.5f + z), Float3(1.0f, 1.0f, 1.0f) };//左下　前
+
+		setVertex[27] = { Float3(0.5f + x, 0.5f + y, 0.5f + z), Float3(0.0f, 1.0f, 1.0f) };//左下　前
+		setVertex[28] = { Float3(0.5f + x,-0.5f + y,-0.5f + z), Float3(1.0f, 1.0f, 1.0f) };//左上　奥
+		setVertex[29] = { Float3(0.5f + x, 0.5f + y,-0.5f + z), Float3(0.0f, 1.0f,-0.0f) };//左下　奥
+
+		//底
+		setVertex[30] = { Float3(-0.5f + x, 0.5f + y, 0.5f + z), Float3(0.0f, 1.0f,-0.0f) };//右下　前
+		setVertex[31] = { Float3(0.5f + x, 0.5f + y, 0.5f + z), Float3(1.0f, 1.0f,-0.0f) };//左下　前
+		setVertex[32] = { Float3(-0.5f + x, 0.5f + y,-0.5f + z), Float3(1.0f, 1.0f, 1.0f) };//右下　奥
+
+		setVertex[33] = { Float3(-0.5f + x, 0.5f + y,-0.5f + z), Float3(0.0f, 1.0f, 1.0f) };//右下　奥
+		setVertex[34] = { Float3(0.5f + x, 0.5f + y, 0.5f + z), Float3(1.0f, 1.0f, 1.0f) };//左下　前
+		setVertex[35] = { Float3(0.5f + x, 0.5f + y,-0.5f + z), Float3(0.0f, 1.0f,-0.0f) };//左下　奥
+
+		for (int i = 0; i < 36; i++)
+		{
+			vertices.push_back(setVertex[i]);
+		}
+	}
+
+
+	void SetCullingMode(D3D11_CULL_MODE culingMode)
+	{
+		D3D11_RASTERIZER_DESC rasterizerDesc = {};
+		rasterizerDesc.FillMode = D3D11_FILL_SOLID;//レンダリング時に使用する描画モードを決定する
+		rasterizerDesc.CullMode = culingMode;//指定の方向を向いている三角形が描画されないことを示す
+		App::GetGraphicsDevice().CreateRasterizerState//ラスタライザーステージに動作を支持するラスタライザステートオブジェクトを作成する
+		(
+			&rasterizerDesc, 
+			&rasterizerState
+		);
+	}
+
+	void Apply()
+	{
+		if (vertices.size() > 0)
+		{
+			D3D11_BUFFER_DESC vertexBufferDesc = {};
+			vertexBufferDesc.ByteWidth = sizeof(Vertex) * vertices.size();
+			vertexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+			vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+			D3D11_SUBRESOURCE_DATA vertexSubresourceData = {};
+			vertexSubresourceData.pSysMem = vertices.data();
+			//バッファの初期化
+			vertexBuffer.Release();
+			//バッファ作成
+			App::GetGraphicsDevice().CreateBuffer(&vertexBufferDesc, &vertexSubresourceData,
+				&vertexBuffer);
+		}
+		if (indices.size() > 0)
+		{
+			D3D11_BUFFER_DESC indexBufferDesc = {};
+			indexBufferDesc.ByteWidth = sizeof(int) * indices.size();
+			indexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+			indexBufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+			D3D11_SUBRESOURCE_DATA indexSubresourceData = {};
+			indexSubresourceData.pSysMem = indices.data();
+			//バッファの初期化
+			indexBuffer.Release();
+			//バッファ作成
+			App::GetGraphicsDevice().CreateBuffer
+			(
+				&indexBufferDesc,//記述要素
+				&indexSubresourceData,//初期化要素
+				&indexBuffer//格納用ポインタアドレス
+			);
+		}
+		material.SetBuffer(&constant, sizeof(Constant));
+	}
+	//描画
+	void Draw()
+	{
+		//vertexBufferが正しく作られているかどうかの確認
+		if (vertexBuffer == nullptr)
+			return;
+
+		material.Attach();
+
+		//軸の角度変更　また座標移動　行列の掛け算をしている　掛け方をかえると変になる
+		constant.world = DirectX::XMMatrixTranspose
+		(
+			DirectX::XMMatrixScaling(scale.x, scale.y, scale.z) *
+			DirectX::XMMatrixRotationY(angles.y) *
+			DirectX::XMMatrixRotationX(angles.x) *
+			DirectX::XMMatrixRotationZ(angles.z) *
+			DirectX::XMMatrixTranslation(position.x, position.y, position.z)
+		);
+		App::GetGraphicsContext().RSSetState(rasterizerState);
+
+		UINT stride = sizeof(Vertex);
+		UINT offset = 0;
+		App::GetGraphicsContext().IASetVertexBuffers(0, 1, &vertexBuffer.p,&stride, &offset);
+
+		if (indexBuffer == nullptr)
+		{
+			App::GetGraphicsContext().Draw(vertices.size(), 0);
+		}
+		else
+		{
+			App::GetGraphicsContext().IASetIndexBuffer(indexBuffer, DXGI_FORMAT_R32_UINT, 0);
+			App::GetGraphicsContext().DrawIndexed(indices.size(), 0, 0);
+		}
+	}
+
+private:
+	struct Constant
+	{
+		DirectX::XMMATRIX world;
+	};
+	Constant constant;
+	ATL::CComPtr<ID3D11Buffer> vertexBuffer = nullptr;
+	ATL::CComPtr<ID3D11Buffer> indexBuffer = nullptr;
+	ATL::CComPtr<ID3D11RasterizerState> rasterizerState = nullptr;
+};
